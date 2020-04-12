d499af6a7f3755dc899c3a10e4b985e17f16343e
Ricardas Navickas <ricardasnv@hotmail.com>
2020-04-12 13:59:44 +0300

    Modify script to push string directly to stack without using any registers

--- push-string-to-stack.sh
+++ push-string-to-stack.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Quick and dirty tool for generating instructions to push a long string on the stack (for x86 or x86_64)
-# Usage: ./push-string-to-stack.sh <file containing string> <cluster size in bytes> <register to use> <indentation>
-# Example: ./push-string-to-stack.sh filename 8 rax "\t" | xclip
+# Usage: ./push-string-to-stack.sh <file containing string> <cluster size in bytes> <indentation>
+# Example: ./push-string-to-stack.sh filename 8 "\t" | xclip
 
 file=$1
 cluster=$(expr 2 \* ${2})
-register=$3
-indent=$4
+indent=$3
 
 # Turn string into sequence of raw bytes and apply padding so that byte count is a multiple of $cluster
 padding_len=$(expr 2 \* \( $cluster - $(wc -c $file | awk '{print $1}') % $cluster \))
@@ -18,5 +17,5 @@ rawbytes=$(xxd -g 1 $file | cut -c 11-57 | sed 's/\ //g' | tr -d "\n")$padding
 revbytes=$(echo $rawbytes | sed 's/\([0-9|a-f]\{2\}\)/\1\n/g' | tac | tr -d "\n")
 
 # Push in clusters of $2 bytes and apply indentation for easy copy-pasting
-printf $revbytes | sed "s/\\([0-9|a-f]\{${cluster}\}\\)/0x\1\\n/g" | awk "{print \"${indent}mov $register, \" \$1 \"\n${indent}push $register\"}"
+printf $revbytes | sed "s/\\([0-9|a-f]\{${cluster}\}\\)/0x\1\\n/g" | awk "{print \"${indent}push \" \$1}"
 
